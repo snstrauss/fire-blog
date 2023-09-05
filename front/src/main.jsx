3c91fcae03fd2ee8b3aa54644cc58e02c3106495
snstrauss <zahavi.guy.85@gmail.com>
2023-09-05 14:56:40 +0300

    added 1 to test commit with credentials

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,5 +2,5 @@ import ReactDOM from "react-dom/client";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <h1>fire blog app</h1>
+  <h1>fire blog app 1</h1>
 );
