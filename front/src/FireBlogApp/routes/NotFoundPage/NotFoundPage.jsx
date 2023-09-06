32eec06f6d6b7fd94bc9d32d4d7fea558f86c5b0
snstrauss <zahavi.guy.85@gmail.com>
2023-09-06 18:48:39 +0300

    use new styling mixin in 'not found' page

--- front/src/FireBlogApp/routes/NotFoundPage/NotFoundPage.jsx
+++ front/src/FireBlogApp/routes/NotFoundPage/NotFoundPage.jsx
@@ -1,15 +1,11 @@
 import S from "./NotFoundPage.module.scss";
-import { useRouteError } from "react-router-dom";
 
 export default function NotFoundPage() {
-  const { statusText, message } = useRouteError();
-
   return (
     <div className={S.container}>
       <h1 className={S.emoji}>🤔</h1>
       <h1>hmm...</h1>
       <p>I'm sure it was somewhere here...</p>
-      <p className={S.statusText}>{statusText || message}</p>
     </div>
   );
 }
