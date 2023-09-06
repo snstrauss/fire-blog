bd05501db3ed6f680c0568da6c1bad8bd336aa94
snstrauss <zahavi.guy.85@gmail.com>
2023-09-06 22:12:43 +0300

    built and styled <HomePage>

--- front/src/FireBlogApp/routes/HomePage/HomePage.jsx
+++ front/src/FireBlogApp/routes/HomePage/HomePage.jsx
@@ -1,5 +1,26 @@
+import S from "./HomePage.module.scss";
+import { Link } from "react-router-dom";
+import fireVideoSrc from "../../assets/video/fire.mp4";
+import { makeNamespacedTypography } from "../../components/Typography/Typography";
+import Button from "../../components/Button/Button";
+
+const HomePageText = makeNamespacedTypography("home");
+
 export default function HomePage() {
   return (
-    <h1>home page</h1>
+    <div className={S.homePage}>
+      <div className={S.videoContainer}>
+        <video src={fireVideoSrc} loop autoPlay muted />
+        <div className={S.textContainer}>
+          <HomePageText className={S.title} path="title" bold />
+          <HomePageText path="subtitle" role="h2" />
+          <Link to="/blog">
+            <Button>
+              <HomePageText path="button" />
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
