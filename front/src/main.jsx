e8e14556944ed2873583f9d5ecb23894e582bf87
snstrauss <zahavi.guy.85@gmail.com>
2023-09-06 19:40:36 +0300

    added some color css variables and gave generated css modules classnames better structure

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import './base.scss';
+import './FireBlogApp/common/variables.scss';
 import FireBlogApp from "./FireBlogApp/FireBlogApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<FireBlogApp />);
