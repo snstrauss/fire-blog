8c64bd9312ae7aada1ea2723c0969403972f5f6c
snstrauss <zahavi.guy.85@gmail.com>
2023-09-06 20:28:39 +0300

    use <Typography> in not-found page

--- front/src/FireBlogApp/routes/NotFoundPage/NotFoundPage.jsx
+++ front/src/FireBlogApp/routes/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,14 @@
+import { makeNamespacedTypography } from "../../components/Typography/Typography";
 import S from "./NotFoundPage.module.scss";
+
+const NotFoundText = makeNamespacedTypography('not_found');
 
 export default function NotFoundPage() {
   return (
     <div className={S.container}>
       <h1 className={S.emoji}>🤔</h1>
-      <h1>hmm...</h1>
-      <p>I'm sure it was somewhere here...</p>
+      <NotFoundText path="title" role="h2" bold />
+      <NotFoundText path="subtitle" />
     </div>
   );
 }
