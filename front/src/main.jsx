a4d260c0a848f71a4cf056ad2cc487b15d86928a
snstrauss <zahavi.guy.85@gmail.com>
2023-09-07 18:54:04 +0300

    replaced google fonts with self hosted fonts from fontsource

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,6 +1,11 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import './FireBlogApp/common/variables.scss';
+import '@fontsource-variable/rubik';
+import '@fontsource-variable/playfair-display';
+import '@fontsource/suez-one';
+import '@fontsource/abril-fatface';
+import '@fontsource-variable/heebo';
 import FireBlogApp from "./FireBlogApp/FireBlogApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<FireBlogApp />);
