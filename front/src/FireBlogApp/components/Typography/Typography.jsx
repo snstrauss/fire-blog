2b20a25387e080240fc878349357c2bffbd33bfc
snstrauss <zahavi.guy.85@gmail.com>
2023-09-07 18:04:30 +0300

    <Typography> can now receive the 'title' role, as well as custom style parameters and template replacement options

--- front/src/FireBlogApp/components/Typography/Typography.jsx
+++ front/src/FireBlogApp/components/Typography/Typography.jsx
@@ -11,22 +11,15 @@ const typographyPropTypes = {
   path: PropTypes.string,
   className: PropTypes.string,
   bold: PropTypes.bool,
+  replace: PropTypes.object,
   override: PropTypes.string,
-  role: PropTypes.oneOf(["body", "h1", "h2", "h3"]),
+  styleParams: PropTypes.object,
+  role: PropTypes.oneOf(["body", "h1", "h2", "h3", "title"]),
 };
 
 export function makeNamespacedTypography(namespace) {
-  function NameSpacedTypography({ path, className, role, bold, override }) {
-    return (
-      <Typography
-        namespace={namespace}
-        path={path}
-        className={className}
-        role={role}
-        bold={bold}
-        override={override}
-      />
-    );
+  function NameSpacedTypography(typographyProps) {
+    return <Typography namespace={namespace} {...typographyProps} />;
   }
 
   NameSpacedTypography.propTypes = typographyPropTypes;
@@ -40,26 +33,38 @@ export default function Typography({
   className = "",
   role = "body",
   bold = false,
+  replace,
   override,
+  styleParams = {}
 }) {
   const { text: allTranslations } = useContext(LocalisationContext);
   const texts = useLocalisation(namespace);
 
-  const textToRender = useMemo(
-    () =>
-      override
-        ? override
-        : path in texts
-        ? texts[path]
-        : allTranslations.misc.errors.missing_text,
-    [path, texts, allTranslations, override]
-  );
+  const textToRender = useMemo(() => {
+    const text = override
+      ? override
+      : path in texts
+      ? texts[path]
+      : allTranslations.misc.errors.missing_text;
+
+    const textWithReplacements = replace
+      ? addParametersToText(text, replace)
+      : text;
+
+    return textWithReplacements;
+  }, [path, texts, allTranslations, replace, override]);
 
   const textClassname = `${S.text} ${className} role-${role} ${
     bold ? "bold" : ""
   }`;
 
-  return <span className={textClassname}>{textToRender}</span>;
+  return <span className={textClassname} style={styleParams}>{textToRender}</span>;
 }
 
 Typography.propTypes = typographyPropTypes;
+
+function addParametersToText(template, replacements) {
+  return Object.entries(replacements).reduce((text, [key, value]) => {
+    return text.replace(`{{${key}}}`, value);
+  }, template);
+}
